9355e1c2c2dd8ac9fbdfc37b2a65ab3b168a69e3
wfhtqp <1006954237@qq.com>
2021-08-20 14:32:49 +0800

    map c-c to esc

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -1,6 +1,9 @@
 local M = {}
 
 M.config = function ()
+  lvim.keys.insert_mode["<C-c>"] = "<esc>"
+  lvim.keys.normal_mode["<C-c>"] = "<esc>"
+  lvim.keys.visual_mode["<C-c>"] = "<esc>"
 end
 
 return M
