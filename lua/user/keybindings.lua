0bffa3f650555234928a19b0c377a87fadd3732c
nzlov <wfhtqp@gmail.com>
2021-08-22 01:13:47 +0800

    add keybindings for dap

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -10,7 +10,16 @@ M.config = function ()
 
 
   if lvim.builtin.dap.active then
-    lvim.keys.normal_mode["<C-g>"] = ":lua require'dapui'.toggle()<cr>"
+    lvim.keys.normal_mode["<M-g>"] = ":lua require'dapui'.toggle()<cr>"
+    lvim.keys.normal_mode["<M-t>"] = ":lua require'dap'.toggle_breakpoint()<cr>"
+    lvim.keys.normal_mode["<M-b>"] = ":lua require'dap'.step_back()<cr>"
+    lvim.keys.normal_mode["<M-c>"] = ":lua require'dap'.continue()<cr>"
+    lvim.keys.normal_mode["<M-r>"] = ":lua require'dap'.run_to_cursor()<cr>"
+    lvim.keys.normal_mode["<M-i>"] = ":lua require'dap'.step_into()<cr>"
+    lvim.keys.normal_mode["<M-o>"] = ":lua require'dap'.step_over()<cr>"
+    lvim.keys.normal_mode["<M-u>"] = ":lua require'dap'.step_out()<cr>"
+    lvim.keys.normal_mode["<M-q>"] = ":lua require'dap'.close()<cr>"
+
   end
 
 end
