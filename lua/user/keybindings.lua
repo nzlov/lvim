ce70996a39083d5df43fcc13590e5b2d3934562c
wfhtqp <1006954237@qq.com>
2021-08-23 09:18:03 +0800

    fix

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -10,16 +10,15 @@ M.config = function ()
 
 
   if lvim.builtin.dap.active then
-    lvim.keys.normal_mode["<M-g>"] = ":lua require'dapui'.toggle()<cr>"
-    lvim.keys.normal_mode["<M-t>"] = ":lua require'dap'.toggle_breakpoint()<cr>"
-    lvim.keys.normal_mode["<M-b>"] = ":lua require'dap'.step_back()<cr>"
-    lvim.keys.normal_mode["<M-c>"] = ":lua require'dap'.continue()<cr>"
-    lvim.keys.normal_mode["<M-r>"] = ":lua require'dap'.run_to_cursor()<cr>"
-    lvim.keys.normal_mode["<M-i>"] = ":lua require'dap'.step_into()<cr>"
-    lvim.keys.normal_mode["<M-o>"] = ":lua require'dap'.step_over()<cr>"
-    lvim.keys.normal_mode["<M-u>"] = ":lua require'dap'.step_out()<cr>"
-    lvim.keys.normal_mode["<M-q>"] = ":lua require'dap'.close()<cr>"
-
+    lvim.keys.normal_mode["<M-g>"] = "<cmd>lua require'dapui'.toggle()<cr>"
+    lvim.keys.normal_mode["<M-t>"] = "<cmd>lua require'dap'.toggle_breakpoint()<cr>"
+    lvim.keys.normal_mode["<M-b>"] = "<cmd>lua require'dap'.step_back()<cr>"
+    lvim.keys.normal_mode["<M-c>"] = "<cmd>lua require'dap'.continue()<cr>"
+    lvim.keys.normal_mode["<M-r>"] = "<cmd>lua require'dap'.run_to_cursor()<cr>"
+    lvim.keys.normal_mode["<M-i>"] = "<cmd>lua require'dap'.step_into()<cr>"
+    lvim.keys.normal_mode["<M-o>"] = "<cmd>lua require'dap'.step_over()<cr>"
+    lvim.keys.normal_mode["<M-u>"] = "<cmd>lua require'dap'.step_out()<cr>"
+    lvim.keys.normal_mode["<M-q>"] = "<cmd>lua require'dap'.close()<cr>"
   end
 
 end
