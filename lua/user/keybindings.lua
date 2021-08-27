f7ca8b2125bf38d718c69bd3bf334b82d109348e
wfhtqp <1006954237@qq.com>
2021-08-27 17:32:56 +0800

    fix key

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -8,8 +8,11 @@ M.config = function ()
 
   lvim.keys.visual_mode["<C-c>"] = "<esc>"
 
+  lvim.keys.normal_mode["[d"] =  "<cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts = {border = lvim.lsp.popup_border}})<cr>"
+  lvim.keys.normal_mode["]d"] =  "<cmd>lua vim.lsp.diagnostic.goto_next({popup_opts = {border = lvim.lsp.popup_border}})<cr>"
 
   if lvim.builtin.dap.active then
+    lvim.builtin.which_key.mappings["dw"] = { "<cmd>lua require'dapui'.toggle()<cr>", "Dap UI" }
     lvim.keys.normal_mode["<M-g>"] = "<cmd>lua require'dapui'.toggle()<cr>"
     lvim.keys.normal_mode["<M-h>"] = "<cmd>lua require'dapui'.eval()<cr>"
     lvim.keys.normal_mode["<M-t>"] = "<cmd>lua require'dap'.toggle_breakpoint()<cr>"
